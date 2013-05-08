bcdb03eb7b25314acaee3f88bd236e10227afa13
jayrambhia <jayrambhia777@gmail.com>
2013-05-08 14:40:02 +0530

    opencv 2.4.5 script added
    Signed-off-by: jayrambhia <jayrambhia777@gmail.com>

--- Ubuntu/2.4/opencv2_4_5.sh
+++ Ubuntu/2.4/opencv2_4_5.sh
@@ -4,7 +4,7 @@ flag=1
 else
 flag=0
 fi
-echo "Installing OpenCV 2.4.5"
+echo "Installing OpenCV 2.4.4"
 mkdir OpenCV
 cd OpenCV
 echo "Removing any pre-installed ffmpeg and x264"
@@ -19,11 +19,11 @@ sudo apt-get -y install libtbb-dev
 sudo apt-get -y install libqt4-dev libgtk2.0-dev
 sudo apt-get -y install libfaac-dev libmp3lame-dev libopencore-amrnb-dev libopencore-amrwb-dev libtheora-dev libvorbis-dev libxvidcore-dev
 sudo apt-get -y install x264 v4l-utils ffmpeg
-echo "Downloading OpenCV 2.4.5"
-wget -O OpenCV-2.4.5.tar.gz http://sourceforge.net/projects/opencvlibrary/files/opencv-unix/2.4.5/opencv-2.4.5.tar.gz/download
-echo "Installing OpenCV 2.4.5"
-tar -xvf OpenCV-2.4.5.tar.gz
-cd OpenCV-2.4.5
+echo "Downloading OpenCV 2.4.4"
+wget -O OpenCV-2.4.4.tar.bz2 http://sourceforge.net/projects/opencvlibrary/files/opencv-unix/2.4.4/OpenCV-2.4.4.tar.bz2/download
+echo "Installing OpenCV 2.4.4"
+tar -xvf OpenCV-2.4.4.tar.bz2
+cd OpenCV-2.4.4
 mkdir build
 cd build
 cmake -D CMAKE_BUILD_TYPE=RELEASE -D CMAKE_INSTALL_PREFIX=/usr/local -D WITH_TBB=ON -D BUILD_NEW_PYTHON_SUPPORT=ON -D WITH_V4L=ON -D INSTALL_C_EXAMPLES=ON -D INSTALL_PYTHON_EXAMPLES=ON -D BUILD_EXAMPLES=ON -D WITH_QT=ON -D WITH_OPENGL=ON ..
