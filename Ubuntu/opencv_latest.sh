4d478db9fa0a2c1c643c8db9e15d1fd10c22cc19
Luis Arias <kaaloo@gmail.com>
2013-10-17 09:48:01 +0200

    Use -j 4 instead of unlimited processes on make

--- Ubuntu/opencv_latest.sh
+++ Ubuntu/opencv_latest.sh
@@ -14,7 +14,7 @@ cd opencv-$version
 mkdir build
 cd build
 cmake -D CMAKE_BUILD_TYPE=RELEASE -D CMAKE_INSTALL_PREFIX=/usr/local -D WITH_TBB=ON -D BUILD_NEW_PYTHON_SUPPORT=ON -D WITH_V4L=ON -D INSTALL_C_EXAMPLES=ON -D INSTALL_PYTHON_EXAMPLES=ON -D BUILD_EXAMPLES=ON -D WITH_QT=ON -D WITH_OPENGL=ON ..
-make -j
+make -j 4
 sudo make install
 sudo sh -c 'echo "/usr/local/lib" > /etc/ld.so.conf.d/opencv.conf'
 sudo ldconfig
