01f6cf8f658fa1020d46f0ad9900237e6317c804
Andres More <more.andres@gmail.com>
2014-02-25 12:09:04 -0500

    cleanup

--- Ubuntu/opencv_install.sh
+++ Ubuntu/opencv_install.sh
@@ -16,26 +16,26 @@ if [[ -z "$dldir" ]]; then
         dldir=OpenCV
 fi
 echo "Installing OpenCV" $version
-mkdir $dldir
+mkdir -p $dldir
 cd $dldir
 echo "Removing any pre-installed ffmpeg and x264"
-sudo apt-get -qq remove ffmpeg x264 libx264-dev
-echo "Installing Dependenices"
-sudo apt-get -qq install libopencv-dev build-essential checkinstall cmake pkg-config yasm libtiff4-dev libjpeg-dev libjasper-dev libavcodec-dev libavformat-dev libswscale-dev libdc1394-22-dev libxine-dev libgstreamer0.10-dev libgstreamer-plugins-base0.10-dev libv4l-dev python-dev python-numpy libtbb-dev libqt4-dev libgtk2.0-dev libfaac-dev libmp3lame-dev libopencore-amrnb-dev libopencore-amrwb-dev libtheora-dev libvorbis-dev libxvidcore-dev x264 v4l-utils ffmpeg unzip
+sudo apt-get remove ffmpeg x264 libx264-dev -y
+echo "Installing Dependencies"
+sudo apt-get install libopencv-dev build-essential checkinstall cmake pkg-config yasm libtiff4-dev libjpeg-dev libjasper-dev libavcodec-dev libavformat-dev libswscale-dev libdc1394-22-dev libxine-dev libgstreamer0.10-dev libgstreamer-plugins-base0.10-dev libv4l-dev python-dev python-numpy libtbb-dev libqt4-dev libgtk2.0-dev libfaac-dev libmp3lame-dev libopencore-amrnb-dev libopencore-amrwb-dev libtheora-dev libvorbis-dev libxvidcore-dev x264 v4l-utils ffmpeg unzip -y
 echo "Downloading OpenCV" $version
-wget -O $downloadfile http://sourceforge.net/projects/opencvlibrary/files/opencv-unix/$version/$downloadfile/download
+wget -c -O $downloadfile http://sourceforge.net/projects/opencvlibrary/files/opencv-unix/$version/$downloadfile/download
 echo "Installing OpenCV" $version
 echo $downloadfile | grep ".zip"
 if [ $? -eq 0 ]; then
-	unzip $downloadfile
+	unzip -u $downloadfile
 else
 	tar -xvf $downloadfile
 fi
 cd opencv-$version
-mkdir build
+mkdir -p build
 cd build
 cmake -D CMAKE_BUILD_TYPE=RELEASE -D CMAKE_INSTALL_PREFIX=/usr/local -D WITH_TBB=ON -D BUILD_NEW_PYTHON_SUPPORT=ON -D WITH_V4L=ON -D INSTALL_C_EXAMPLES=ON -D INSTALL_PYTHON_EXAMPLES=ON -D BUILD_EXAMPLES=ON -D WITH_QT=ON -D WITH_OPENGL=ON ..
-make -j 4
+make -j `grep -c processor /proc/cpuinfo`
 sudo make install
 sudo sh -c 'echo "/usr/local/lib" > /etc/ld.so.conf.d/opencv.conf'
 sudo ldconfig
