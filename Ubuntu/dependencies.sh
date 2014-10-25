ba4fce4dfc0db32635b79ec41d372a28f5efa171
Arky <hitmanarky@gmail.com>
2014-10-25 19:40:13 +0700

    Changes for Ubuntu Utopic 14.10

--- Ubuntu/dependencies.sh
+++ Ubuntu/dependencies.sh
@@ -14,14 +14,14 @@ install_dependency checkinstall
 install_dependency cmake
 install_dependency pkg-config
 install_dependency yasm
-install_dependency libtiff4-dev
+install_dependency libtiff5-dev
 install_dependency libjpeg-dev
 install_dependency libjasper-dev
 install_dependency libavcodec-dev
 install_dependency libavformat-dev
 install_dependency libswscale-dev
 install_dependency libdc1394-22-dev
-install_dependency libxine-dev
+install_dependency libxine2-dev
 install_dependency libgstreamer0.10-dev
 install_dependency libgstreamer-plugins-base0.10-dev
 install_dependency libv4l-dev
@@ -39,5 +39,5 @@ install_dependency libvorbis-dev
 install_dependency libxvidcore-dev
 install_dependency x264
 install_dependency v4l-utils
-install_dependency ffmpeg
+#install_dependency ffmpeg
 install_dependency unzip
